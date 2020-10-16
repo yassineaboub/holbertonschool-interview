a6654f5d8ce686ab0bff4ce9155ccbbbabd99076
root <yassineaboub@live.fr>
2020-10-16 11:46:16 +0000

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,7 +3,7 @@ def canUnlockAll(boxes):
     k = [0]
     for i in k:
         for n in boxes[i]:
-            if n not in k and n > 0:
+            if n not in k and n > 0 and n < len(boxes):
                 k.append(n)
     if len(k) == len(boxes):
         return True
