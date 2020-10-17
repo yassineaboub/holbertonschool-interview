13c76c3949ef4289b6f4caa8cd024bab81243269
root <yassineaboub@live.fr>
2020-10-17 09:07:04 +0000

    0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
 """Minimum Operations"""
+
 def minOperations(n):
+""" Function that returns the minimum number of operations to be performed """
+
     if n <= 1:
         return 0
     count = 0
     j = 2
     while (n > 1):
-        """ if n is divisible by 2"""
+        """if n is divisible by 2"""
         if (n % j == 0):
             n //= j
             count += j
