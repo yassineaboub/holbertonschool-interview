61e923514f32249274ce860de463f2e5e4336e6c
root <yassineaboub@live.fr>
2021-04-07 22:19:07 +0000

    0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const args = process.argv.slice(2);
+const args = process.argv[2];
 
 request.get(`https://swapi-api.hbtn.io/api/films/${args}/`, async (err, response, body) => {
   const char = JSON.parse(body).char;
