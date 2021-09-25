a633ebaa4c6e02030983c799b4c9b1a12fc3a40b
yassineaboub <yassineaboub@live.fr>
2021-09-25 14:27:14 +0100

    wild

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - func that compare two strings
+ * @s1: string to compare
+ * @s2: string to compare that can have wildcards
+ * Return: 1 if strings are identical else 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (!*s2);
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
